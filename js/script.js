3f1490c6dc73c51ba5ccd239c628d4cfef4487ac
Milosz Anders <milosz.anders@gmail.com>
2023-12-12 19:41:39 +0100

    JS functions refactoring

--- js/script.js
+++ js/script.js
@@ -1,35 +1,43 @@
-let buttonElement = document.querySelector(".js-button");
-let bodyElement = document.querySelector(".js-body");
-let menuNavigationElement = document.querySelector(".js-navigation");
-let footerElement = document.querySelector(".js-footer");
-let mainHeaderElement = document.querySelector('.js-headerTitle');
-var tableCellElement = document.querySelectorAll('.js-tableCell');
-var tableRowElement = document.querySelectorAll('.js-tableRow');
-var menuListItemElement = document.querySelectorAll('.js-navigationItem');
-let themeName = document.querySelector('.js-themeName');
+{
+  const welcome = () => {
+    console.log("Witam wszystkich developerów tutaj zaglądających!");
+  }
 
+  const runHighContrastTheme = () => {
+    const bodyElement = document.querySelector(".js-body");
+    const menuNavigationElement = document.querySelector(".js-navigation");
+    const footerElement = document.querySelector(".js-footer");
+    const mainHeaderElement = document.querySelector('.js-headerTitle');
+    const tableCellElement = document.querySelectorAll('.js-tableCell');
+    const tableRowElement = document.querySelectorAll('.js-tableRow');
+    const menuListItemElement = document.querySelectorAll('.js-navigationItem');
+    const themeName = document.querySelector('.js-themeName');
 
-buttonElement.addEventListener("click", () => {
-  bodyElement.classList.toggle("body--highContrast");
+    bodyElement.classList.toggle("body--highContrast");
+    themeName.innerText = bodyElement.classList.contains("body--highContrast") ? "Włącz motyw podstawowy" : "Włącz tryb wysokiego kontrastu";
+    menuNavigationElement.classList.toggle("navigation--highContrast");
+    footerElement.classList.toggle("footer--highContrast");
+    mainHeaderElement.classList.toggle("header__title--highContrast");
 
-  themeName.innerText = bodyElement.classList.contains("body--highContrast") ? "Włącz motyw podstawowy" : "Włącz tryb wysokiego kontrastu";
+    tableCellElement.forEach(table__cell => {
+      table__cell.classList.toggle("table__cell--highContrast");
+    });
 
-  menuNavigationElement.classList.toggle("navigation--highContrast");
-  footerElement.classList.toggle("footer--highContrast");
-  mainHeaderElement.classList.toggle("header__title--highContrast");
+    tableRowElement.forEach(table__row => {
+      table__row.classList.toggle("table__row--highContrast");
+    });
 
+    menuListItemElement.forEach(menuNavigation__menuListItem => {
+      menuNavigation__menuListItem.classList.toggle("navigation__item--hightContrast");
+    });
+  };
 
-  tableCellElement.forEach(function (table__cell) {
-    table__cell.classList.toggle("table__cell--highContrast");
-  });
+  const init = () => {
+    const buttonElement = document.querySelector(".js-button");
+    buttonElement.addEventListener("click", runHighContrastTheme);
 
+    welcome();
+  };
 
-  tableRowElement.forEach(function (table__row) {
-    table__row.classList.toggle("table__row--highContrast");
-  });
-
-  menuListItemElement.forEach(function (menuNavigation__menuListItem) {
-    menuNavigation__menuListItem.classList.toggle("navigation__item--hightContrast");
-  });
-
-});
+  init();
+}
